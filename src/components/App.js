bd9f0a1b67736a1008fac0d4948594661cc2e46d
ldv432 <ldv432@gmail.com>
2024-04-30 12:39:35 -0600

    did it

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
+
+
 function App() {
+  const [tasksState, setTasksState] = useState(TASKS)
+  const [categoriesState, setCategoriesState] = useState(CATEGORIES)
+  const [chosenCategory, setChosenCategory] = useState("All")
+
+  function handleDeleteTask(deletedTask){
+    const filteredTasks = tasksState.filter(task => task !== deletedTask)
+    setTasksState(filteredTasks)
+  }
+
+  function filterCategory(buttonCategory){
+    setChosenCategory(buttonCategory)
+    const filteredCategories = TASKS.filter(task => task.category === buttonCategory || buttonCategory === "All") 
+
+    setTasksState(filteredCategories)
+  }
+
+
+  function handleNewTask(newTask){
+    setTasksState([...tasksState, newTask])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={categoriesState} filterCategory={filterCategory} chosenCategory={chosenCategory}/>
+      <NewTaskForm categories={categoriesState} onTaskFormSubmit={handleNewTask}/>
+      <TaskList tasks={tasksState} handleDeleteTask={handleDeleteTask}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter( {categories, filterCategory, chosenCategory} ) {
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map(category =>{
+        const handleFilter = () => {
+          filterCategory(category)
+        }
+        return <button key={category} onClick={handleFilter} className={category === chosenCategory ? "selected" : ""}>{category}</button>
+      })}
     </div>
   );
 }
